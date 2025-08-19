fac6a51e213cfc57f317e34316f14512d736e8bf
Franco Eusébio Garcia <francoegarcia@outlook.com>
2025-08-19 11:07:52 -0300

    More examples.

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
-/* gcc main.c && ./a.out */
-
 #include <stdio.h>
 #define 🖨️ printf
+#define 🧮(X) ((X) * (X))
+#define ²
+
+void 👀(unsigned char 🕶️) {
+    if (!🕶️) {
+        return;
+    }
+
+    🖨️("%d\n", 🕶️);
+
+    👀(🕶️ - 1);
+}
 
 int main() {
     const int 💯 = 100;
     int 🖩 = 💯 + 💯;
     🖨️("%d\n", 🖩);
+
+    🖩 = 🧮(💯)²;
+    🖨️("%d\n", 🖩);
+
+    👀(💯 / 💯);
 
     return 0;
 }
